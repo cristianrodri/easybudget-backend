2b494e67398f5ab3c0c3a74fecdba66f092b48e9
Cristian <contacto.cristianrodriguez@gmail.com>
2021-07-18 11:35:35 -0300

    delete user avatar after user is deleting

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -4,12 +4,22 @@ module.exports = {
    */
   lifecycles: {
     async afterDelete(result, params) {
+      // delete avatar
+      if (result.avatar?.id) {
+        const file = await strapi.plugins["upload"].services.upload.fetch({
+          id: result.avatar.id,
+        });
+        await strapi.plugins["upload"].services.upload.remove(file);
+        console.log("Avatar deleted after deleting user");
+      }
+
       // delete all budgets related to the deleted user
       result.budgets.forEach((entity) => {
         strapi.services.budget.delete({
           id: entity.id,
         });
       });
+      console.log("Budgets deleted after deleting user");
     },
   },
 };
