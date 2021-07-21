b19f068fd955caaacc80c9fb6199c3501026bb4c
Cristian <contacto.cristianrodriguez@gmail.com>
2021-07-21 01:56:22 -0300

    delete user data by checking if the requested was the admin or user

--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -4,22 +4,44 @@ module.exports = {
    */
   lifecycles: {
     async afterDelete(result, params) {
-      // delete avatar
-      if (result.avatar?.id) {
-        const file = await strapi.plugins["upload"].services.upload.fetch({
-          id: result.avatar.id,
-        });
-        await strapi.plugins["upload"].services.upload.remove(file);
-        console.log("Avatar deleted after deleting user");
-      }
+      // check if result is an array, otherwise is an object (data.length === undefined)
+      if (typeof result.length === "number") {
+        // code used when data is removed by admin
+        result.forEach(async (user) => {
+          // delete avatar
+          if (user.avatar?.id) {
+            const file = await strapi.plugins["upload"].services.upload.fetch({
+              id: user.avatar.id,
+            });
+            await strapi.plugins["upload"].services.upload.remove(file);
+          }
 
-      // delete all budgets related to the deleted user
-      result.budgets.forEach((entity) => {
-        strapi.services.budget.delete({
-          id: entity.id,
+          // delete all budgets related to the deleted user
+          user.budgets.forEach((entity) => {
+            strapi.services.budget.delete({
+              id: entity.id,
+            });
+          });
         });
-      });
-      console.log("Budgets deleted after deleting user");
+      } else {
+        // code used when data is removed by user
+        // delete avatar
+        if (result.avatar?.id) {
+          const file = await strapi.plugins["upload"].services.upload.fetch({
+            id: result.avatar.id,
+          });
+          await strapi.plugins["upload"].services.upload.remove(file);
+          console.log("Avatar deleted after deleting user");
+        }
+
+        // delete all budgets related to the deleted user
+        result.budgets.forEach((entity) => {
+          strapi.services.budget.delete({
+            id: entity.id,
+          });
+        });
+        console.log("Budgets deleted after deleting user");
+      }
     },
   },
 };
