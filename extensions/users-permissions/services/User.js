219783c66ae14744e7adc4a207a08a0d7b54eb26
Cristian <contacto.cristianrodriguez@gmail.com>
2021-08-12 15:40:03 -0300

    only get role collection when user is updated

--- extensions/users-permissions/services/User.js
+++ extensions/users-permissions/services/User.js
@@ -2,6 +2,20 @@
 
 module.exports = {
   /**
+   * Promise to edit a/an user.
+   * @return {Promise}
+   */
+  async edit(params, values) {
+    if (values.password) {
+      values.password = await strapi.plugins[
+        "users-permissions"
+      ].services.user.hashPassword(values);
+    }
+
+    await strapi.query("user", "users-permissions").update(params, values);
+    return strapi.query("user", "users-permissions").findOne(params, ["role"]);
+  },
+  /**
    * Promise to fetch authenticated user.
    * @return {Promise}
    */
