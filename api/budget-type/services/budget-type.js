b493d9c48ea5ecd4375498ebdffec35c25d6eed7
Cristian <contacto.cristianrodriguez@gmail.com>
2021-08-12 15:39:27 -0300

    get budget-type without user and budget data

--- api/budget-type/services/budget-type.js
+++ api/budget-type/services/budget-type.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async update(params, data) {
+    await strapi.query("budget-type").update(params, data);
+
+    // after updating get data without user and budget collections
+    return strapi.query("budget-type").findOne(params, []);
+  },
+  find(params) {
+    return strapi.query("budget-type").find(params, []);
+  },
+  findOne(params) {
+    return strapi.query("budget-type").findOne(params, []);
+  },
+};
