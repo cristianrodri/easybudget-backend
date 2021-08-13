51c7a18c2221b94b37a8604ab3e8e4b6069d0452
Cristian <contacto.cristianrodriguez@gmail.com>
2021-08-13 15:32:35 -0300

    show budget data without user and budget types data

--- api/budget/services/budget.js
+++ api/budget/services/budget.js
@@ -1,8 +1,35 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const { isDraft } = require("strapi-utils").contentTypes;
+
+module.exports = {
+  async create(data) {
+    const isDraftChecked = isDraft(data, strapi.models.budget);
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      strapi.models.budget,
+      data,
+      { isDraftChecked }
+    );
+
+    const entry = await strapi.query("budget").create(validData);
+
+    return this.findOne({ id: entry.id }, []);
+  },
+  async update(params, data) {
+    await strapi.query("budget").update(params, data);
+
+    // after updating get data without user and budget collections
+    return strapi.query("budget").findOne(params, []);
+  },
+  find(params) {
+    return strapi.query("budget").find(params, []);
+  },
+  findOne(params) {
+    return strapi.query("budget").findOne(params, []);
+  },
+};
