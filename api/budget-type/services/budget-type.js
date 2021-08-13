8a11db58f03bda0088efe04e0e33a93f96a1eb72
Cristian <contacto.cristianrodriguez@gmail.com>
2021-08-13 15:33:32 -0300

    show budget type data without user and budgets after type is created

--- api/budget-type/services/budget-type.js
+++ api/budget-type/services/budget-type.js
@@ -5,7 +5,21 @@
  * to customize this service
  */
 
+const { isDraft } = require("strapi-utils").contentTypes;
+
 module.exports = {
+  async create(data) {
+    const isDraftChecked = isDraft(data, strapi.models["budget-type"]);
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      strapi.models["budget-type"],
+      data,
+      { isDraftChecked }
+    );
+
+    const entry = await strapi.query("budget-type").create(validData);
+
+    return this.findOne({ id: entry.id }, []);
+  },
   async update(params, data) {
     await strapi.query("budget-type").update(params, data);
 
